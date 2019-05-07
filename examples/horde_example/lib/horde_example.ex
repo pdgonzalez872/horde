652500ac8bbfda03f3dedfe1e57f11faa31ef73a
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2019-05-07 15:03:46 -0500

    Fix typo

--- examples/horde_example/lib/horde_example.ex
+++ examples/horde_example/lib/horde_example.ex
@@ -4,6 +4,6 @@ defmodule HordeExample do
   """
 
   def add_child_to_supervisor() do
-    Horde.Supervisor.start_child(MyApp.DistributedSupervisor, %{id: :gen_server, start: {GenServer, :start_link, []}})
+    Horde.Supervisor.start_child(HordeExample.DistributedSupervisor, %{id: :gen_server, start: {GenServer, :start_link, []}})
   end
 end
